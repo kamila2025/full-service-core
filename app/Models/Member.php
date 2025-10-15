8f0695767d74066646f7aa9a3e6fdb124ed508c9
jianqi <jianqi810@gmail.com>
2025-10-15 13:41:16 +0800

    feat: 租戶會員新增相關個人資料、模型及枚舉

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -3,16 +3,29 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use \Illuminate\Database\Eloquent\SoftDeletes;
 
 class Member extends Model
 {
+  use SoftDeletes;
+
   protected $guarded = [];
 
-  /**
-   * 關聯的 Line 用戶
-   */
+  protected $casts = [
+    'birthday' => 'date',
+    'gender'   => \App\Enums\Tenant\Member\MemberGenderEnum::class,
+    'status'   => \App\Enums\Tenant\Member\MemberStatusEnum::class,
+  ];
+
   public function lineUsers()
   {
     return $this->hasMany(LineUser::class);
+  }
+
+  protected static function booted()
+  {
+    static::creating(function ($model) {
+      $model->status = \App\Enums\Tenant\Member\MemberStatusEnum::正常;
+    });
   }
 }
